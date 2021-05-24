82bcb91eb12cb02f8d4238c0b93c359d581cf8ff
Koushik G <koushikgraj@gmail.com>
2021-05-24 10:13:59 +0530

    24-May-2021 : PHP OOPS

--- 17-OOPS-Magic-methods/03__get.php
+++ 17-OOPS-Magic-methods/03__get.php
@@ -1,10 +1,13 @@
 <?php
 class Student {
-    private $data = array();
+    public $data = array();
+	
+	public $test;
  
     public function __set($name, $value) 
     {
         $this->data[$name] = $value;
+		echo $name . " is  ".$value,"<br>";
     }
  
     public function __get($name) 
@@ -18,8 +21,14 @@ class Student {
 $objStudent = new Student();
  
 //  __set() called
-$objStudent->phone = '0491 570 156';   
+$objStudent->phone = '0491 570 156'; 
+
+$objStudent->test= "Debendra";
+
+$objStudent->test123= "ABCD";
  
 //  __get() called
-echo $objStudent->phone;                      
+echo $objStudent->phone;  
+
+//print_r($objStudent->data);
 ?>
